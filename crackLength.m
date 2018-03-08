70659bb419d79866a45fcf274483fd6b7efe8f68
rkn2 <rkn2@princeton.edu>
2018-03-08 15:22:28 -0500

    point recognition, clustering, and plotting of clusters works

--- crackLength.m
+++ crackLength.m
@@ -1,21 +1,31 @@
 %load image
-img2 = imread('G:\\My Drive\\Documents\\Undergrads\\Anna Blyth\\GreenWorms.jpg');
+img = imread('G:\\My Drive\\Documents\\Undergrads\\Anna Blyth\\GreenWorms.jpg');
 %test image
 %img = imread('C:\\Users\\Rebecca Napolitano\\Downloads\\test.bmp');
 
 %pull out only the green parts
-green = img2(:,:,2) > 128 & img2(:,:,1) < 10;
+green = img(:,:,2) > 128 & img(:,:,1) < 10;
 [yy, xx] = find(green);
 
 %plot the green parts
 figure; 
-imshow(img2);
+imshow(img);
 hold on
-plot(xx, yy, '.')
+%plot(xx, yy, '.')
+greenPoints = [xx,yy]; 
+
 
 %draw lines through each set of cracks
+%Agglomerative clustering with single linkage
+%https://www.mathworks.com/help/matlab/math/creating-and-concatenating-matrices.html
+Z = linkage(greenPoints, 'single'); %figures out which pixels are close to each other
+%works but you have to know how many cracks you have.. so that is not
+%optimal
+%c = cluster(Z, 'maxclust', 100);
+c = cluster(Z, 'cutoff', 0.1);
+unique(c)
 
-
+gscatter(xx,yy,c)
 
  
 
