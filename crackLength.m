fe76241d08284e86c4f749bb10b7f0740a8bc616
rkn2 <rkn2@princeton.edu>
2018-03-09 13:54:34 -0500

    gscatter

--- crackLength.m
+++ crackLength.m
@@ -15,7 +15,7 @@ hold on
 greenPoints = [xx,yy]; 
 
 
-%draw lines through each set of cracks
+%cluster the groups of cracks
 %Agglomerative clustering with single linkage
 %https://www.mathworks.com/help/matlab/math/creating-and-concatenating-matrices.html
 Z = linkage(greenPoints, 'single'); %figures out which pixels are close to each other
@@ -23,9 +23,37 @@ Z = linkage(greenPoints, 'single'); %figures out which pixels are close to each 
 %optimal
 %c = cluster(Z, 'maxclust', 100);
 c = cluster(Z, 'cutoff', 0.1);
-unique(c)
 
-gscatter(xx,yy,c)
+%gscatter(xx,yy,c) %will color the different groups but not by length
+
+
+
+%figure out how long each crack is
+
+%figure out how many unique clusters there are
+numClust = unique(c);
+for i = 1:numClust
+    clusterValue = numClust(i,1);
+    selRow = find(c(:,1) == clusterValue); %finds the rows where that cluster's number is present
+    numRows = length(selRow); %tells us how many rows we are looking at for that cluster
+    
+    XX = zeros(numRows,1); %values from xx in rows indicated by the cluster matrix
+    YY = zeros(numRows,1); %values from yy in rows indicated by the cluster matrix
+    
+    for j = 1:numRows
+        selRowValue = selRow(j,1); %get the number of the row we are interested in
+        XX(j,1) = xx(selRowValue,1); %get the value in xx at that row
+        YY(j,1) = yy(selRowValue,1); %get the value in yy at that row    
+    end
+    %scatter(XX,YY)
+    %line(XX,YY)
+    polyfit(XX,YY,1);
+    XXYY = [XX,YY];
+    d = diff(XXYY);
+    total_length = sum(sqrt(sum(d.*d,2)));
+
+end
+
 
  
 
